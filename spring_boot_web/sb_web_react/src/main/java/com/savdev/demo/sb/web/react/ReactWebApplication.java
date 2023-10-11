b525dc13c94ae3e53fd01efca55f0a76cec1e350
Alexandr <sav.public@yandex.com>
2023-10-11 09:11:04 +0200

    spring boot with react

---
+++ spring_boot_web/sb_web_react/src/main/java/com/savdev/demo/sb/web/react/ReactWebApplication.java
@@ -0,0 +1,13 @@
+package com.savdev.demo.sb.web.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactWebApplication.class, args);
+  }
+
+}
